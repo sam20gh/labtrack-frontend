b23640714e84f1225f4bf07562064578cbd4ccb4
Sam Ghoreishi <sam20gh@gmail.com>
2025-03-19 14:39:56 +0000

    config

--- constants/config.ts
+++ constants/config.ts
@@ -1,1 +1,1 @@
-export const API_URL = 'http://192.168.1.100:5002/api'; // Replace with actual backend URL
+export const API_URL = 'https://labtrack-backend.onrender.com/api'; // Replace with actual backend URL
